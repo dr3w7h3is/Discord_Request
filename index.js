defbd027b198c93b4d907c24d2c821e01abd0163
Drew Theis <32044367+dr3w7h3is@users.noreply.github.com>
2019-10-27 02:29:28 -0700

    Init
    Inital push

---
+++ index.js
@@ -0,0 +1,102 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const request = require('request');
+require('dotenv').config();
+
+const token = process.env.token;
+const ombi = process.env.reqAPI;
+
+client.on('message', message => {
+    client.user.setActivity("Being a POS");
+    let fullCommand = message.content.substr(1) // Remove the leading exclamation mark
+    let splitCommand = fullCommand.split(" ") // Split the message up in to pieces for each space
+    let primaryCommand = splitCommand[0] // The first word directly after the exclamation is the command
+    var args = fullCommand;
+    var str = '!' + primaryCommand;
+    args = args.replace(str, '');
+    
+	if (primaryCommand === 'plex.help') {
+        message.channel.send('Shit dude I\'m fucking lazy nothing works yet, but....' + 
+        '\n!plex.help - Shows Commands' + 
+        '\n!req.movie (Movie Title)' + 
+        '\n!req.tv (Show Name)' +
+        '\n!check.movie (Movie Title)' +
+        '\n!check.tv (Show Name)');
+    } else if (primaryCommand  === 'req.movie') {
+        if (!args.length) {
+            return message.channel.send('You didn\'t provide a Movie title!');
+        }
+        args = args.replace("req.movie", "");
+        message.channel.send('Checking for: ' + args);
+        request({
+            headers: {
+                'ApiKey': ombi
+            },
+            uri: 'http://192.168.0.100:5000/api/v1/Search/movie/' + args,
+            method: 'GET'
+        }, function (err, res, body) {
+            var movie = JSON.parse(body);
+            var movieData = {
+                "theMovieDbId": movie[0].id
+            }
+            request({
+                headers: {
+                    'ApiKey': ombi
+                },
+                uri: 'http://192.168.0.100:5000/api/v1/Request/movie/',
+                method: 'POST',
+                json: movieData
+            }), function (err, res, body) {
+
+            }    
+        });
+    } else if (primaryCommand  === 'req.tv') {
+        if (!args.length) {
+            return message.channel.send('You didn\'t provide a show name!');
+        }
+        message.channel.send('Checking for: ' + argus);
+    } else if (primaryCommand  === 'check.movie') {
+        if (!args.length) {
+            return message.channel.send('You didn\'t provide a Movie title!');
+        }
+        args = args.replace("check.movie", "");
+        message.channel.send('Checking for: ' + args);
+        request({
+            headers: {
+                'ApiKey': ombi
+            },
+            uri: 'http://192.168.0.100:5000/api/v1/Search/movie/' + args,
+            method: 'GET'
+        }, function (err, res, body) {
+            var movie = JSON.parse(body);
+            if (movie[0].available){
+                message.channel.send(movie[0].title + " is availble to view on plex");
+            } else {
+                message.channel.send(movie[0].title + " is unavailable on plex");
+            }      
+        });  
+    } else if (primaryCommand  === 'check.tv') {
+        if (!args.length) {
+            return message.channel.send('You didn\'t provide a show name!');
+        }
+        args = args.replace("check.tv", "");  
+        message.channel.send('Checking for: ' + args); 
+        request({
+            headers: {
+                'ApiKey': ombi
+            },
+            uri: 'http://192.168.0.100:5000/api/v1/Search/tv/' + args,
+            method: 'GET'
+        }, function (err, res, body) {
+            var show = JSON.parse(body);
+            if (show[0].fullyAvailable){
+                message.channel.send(show[0].title + " is availble to view on plex");
+            } else if (show[0].partlyAvailable) {
+                message.channel.send(show[0].title + " has limited episodes on plex");
+            } else {
+                message.channel.send(show[0].title + " is unavailable on plex");
+            }      
+        });   
+    }
+});
+client.login(token);
