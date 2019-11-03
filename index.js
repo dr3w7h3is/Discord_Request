82052aef9f8c489edc9b7b675498204713742cd8
dr3w7h3is <drew.theis.1@gmail.com>
2019-11-02 21:07:52 -0700

    Added logic for multiple entries

--- index.js
+++ index.js
@@ -110,12 +110,35 @@ function checkMovie(message, args) {
         method: 'GET'
     }, function (err, res, body) {
         var movie = JSON.parse(body);
-        if (movie[0].available){
-            message.channel.send(movie[0].title + " is availble to view on plex");
-        } else if (movie[0].requested) {
-            message.channel.send(movie[0].title + " is unavailable but is already requested");
+        if (movie.length == 1) {
+            if (movie[0].available){
+                message.channel.send(movie[0].title + " is availble to view on plex");
+            } else if (movie[0].requested) {
+                message.channel.send(movie[0].title + " is unavailable but is already requested");
+            } else {
+                message.channel.send(movie[0].title + " is unavailable on plex");
+            } 
         } else {
-            message.channel.send(movie[0].title + " is unavailable on plex");
+            message.channel.send("Multiple similar title where found enter the number for the correct movie.");
+            var i;
+            for (i = 0; i < movie.length; i++) {
+                message.channel.send((i + 1) + ") " + movie[i].title + " (" + movie[i].releaseDate.substring(0, 4) + ")");
+            }
+            var res = new Discord.Client();
+            res.on('message', input => {
+                if (message.author.id == input.author.id) {
+                    var choice = input.content - 1;
+                    input.channel.send("Looking up your choice: " + movie[choice].title + " (" + movie[choice].releaseDate.substring(0, 4) + ")" );
+                    if (movie[0].available){
+                        input.channel.send(movie[0].title + " is availble to view on plex");
+                    } else if (movie[0].requested) {
+                        input.channel.send(movie[0].title + " is unavailable but is already requested");
+                    } else {
+                        input.channel.send(movie[0].title + " is unavailable on plex");
+                    } 
+                }
+            })
+            res.login(token);
         }      
     });
 }
@@ -130,13 +153,35 @@ function checkTV(message, args) {
         method: 'GET'
     }, function (err, res, body) {
         var show = JSON.parse(body);
-        console.log(show[0]);
-        if (show[0].fullyAvailable){
-            message.channel.send(show[0].title + " is availble to view on plex");
-        } else if (show[0].partlyAvailable) {
-            message.channel.send(show[0].title + " has limited episodes on plex");
+        if (show.length == 1) {
+            if (show[0].fullyAvailable){
+                message.channel.send(show[0].title + " is availble to view on plex");
+            } else if (show[0].partlyAvailable) {
+                message.channel.send(show[0].title + " has limited episodes on plex");
+            } else {
+                message.channel.send(show[0].title + " is unavailable on plex");
+            }
         } else {
-             message.channel.send(show[0].title + " is unavailable on plex");
+            message.channel.send("Multiple similar title where found enter the number for the correct TV show.");
+            var i;
+            for (i = 0; i < show.length; i++) {
+                message.channel.send((i + 1) + ") " + show[i].title + " (" + show[i].firstAired.substring(0, 4) + ")");
+            }
+            var res = new Discord.Client();
+            res.on('message', input => {
+                if (message.author.id == input.author.id) {
+                    var choice = input.content - 1;
+                    input.channel.send("Looking up your choice: " + show[choice].title + " (" + show[choice].firstAired.substring(0, 4) + ")");
+                    if (show[0].fullyAvailable){
+                        message.channel.send(show[0].title + " is availble to view on plex");
+                    } else if (show[0].partlyAvailable) {
+                        message.channel.send(show[0].title + " has limited episodes on plex");
+                    } else {
+                        message.channel.send(show[0].title + " is unavailable on plex");
+                    } 
+                }
+            })
+            res.login(token);
         }      
     });
 }
